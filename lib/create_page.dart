d0f690b38670c0087b54a8281e9b85cc2048e1a3
gokcearslan <gokcearslann17@gmail.com>
2024-05-07 23:54:42 +0300

    empty save/error message 'adv' added

--- lib/create_page.dart
+++ lib/create_page.dart
@@ -128,6 +128,8 @@ class _IlanWidgetState extends State<IlanWidget> {
   TextEditingController _advertisementTypeController = TextEditingController();
   TextEditingController _advertisementDetailsController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
   List<String> ad_types = [];
   String? _selectedType;
 
@@ -149,25 +151,27 @@ class _IlanWidgetState extends State<IlanWidget> {
 
   Future<void> _loadTypeNames() async {
     ad_types = await getTypeNames();
-    setState(() {}); // Call setState to rebuild the widget with the loaded types
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: 20),
-          Container(
-            width: double.infinity,
-            child: TextFormField(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 20),
+            TextFormField(
               controller: _advertisementNameController,
-              style: const TextStyle(
-                fontSize: 20,
-                color: Colors.black,
-              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Bu alan boş bırakılamaz';
+                }
+                return null;
+              },
               decoration: InputDecoration(
                 labelText: 'İlan başlığı',
                 labelStyle: TextStyle(
@@ -180,32 +184,29 @@ class _IlanWidgetState extends State<IlanWidget> {
                   borderSide: BorderSide.none,
                   borderRadius: BorderRadius.circular(12),
                 ),
-                contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 10), // Increase padding to make the field larger
+                contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 10),
               ),
-            ),
-          ),
-          SizedBox(height: 20),
-          Container(
-            width: double.infinity,
-            child: TextFormField(
-              controller: _advertisementDetailsController,
-              style: const TextStyle(
+              style: TextStyle(
                 fontSize: 20,
                 color: Colors.black,
               ),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              controller: _advertisementDetailsController,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Bu alan boş bırakılamaz';
+                }
+                return null;
+              },
               decoration: InputDecoration(
                 labelText: 'İlan detayları',
                 labelStyle: TextStyle(
                   fontSize: 20,
                   color: textColor,
                 ),
-                // icon: Icon(Icons.details), // Icon next to the label
-                hintText: 'Gelecek olan kişinin payına düşen kira, oda sayısı, eşyalı/eşyasız olma durumu vb. bilgileri giriniz.',
-                hintStyle: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey,
-                ),
-                hintMaxLines: 3,
+                hintText: 'Detayları giriniz',
                 fillColor: backgroundColor,
                 filled: true,
                 border: OutlineInputBorder(
@@ -218,115 +219,90 @@ class _IlanWidgetState extends State<IlanWidget> {
               minLines: 3,
               keyboardType: TextInputType.multiline,
             ),
-          ),
-          SizedBox(height: 20),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              color: backgroundColor,
-            ),
-            child: DropdownButtonHideUnderline(
-              child: DropdownButton<String>(
-                hint: Text('İlan türünü seçiniz'),
-                value: _selectedType,
-                onChanged: (String? newValue) {
-                  if (newValue != null) {
+            SizedBox(height: 20),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: backgroundColor,
+              ),
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton<String>(
+                  hint: Text('İlan türünü seçiniz'),
+                  value: _selectedType,
+                  onChanged: (String? newValue) {
                     setState(() {
                       _selectedType = newValue;
                     });
-                  }
-                },
-                dropdownColor: backgroundColor,
-                items: ad_types.map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(
-                      value,
-                      style: TextStyle(color: textColor, fontSize: 21),
-                    ),
-                  );
-                }).toList(),
-                style: TextStyle(color: textColor, fontSize: 21),
-                icon: Icon(Icons.arrow_drop_down, color: textColor),
-                iconSize: 24,
-                elevation: 16,
-                isExpanded: true,
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          //FOTO EKLEME YERİ
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.add_photo_alternate, size: 50),
-                onPressed: () {
-                  // Functionality to be implemented later
-                  print('Icon to add image pressed');
-                },
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () async {
-              String? userId = FirebaseAuth.instance.currentUser?.uid;
-              if (userId == null) {
-                print('No user logged in');
-                return;
-              }
-              if (_selectedType != null) {
-                Advertisement a = Advertisement(
-                    advertisementName: _advertisementNameController.text,
-                    advertisementType: _selectedType,
-                    advertisementDetails: _advertisementDetailsController.text,
-                    userId: userId
-                );
-                AdvertisementService().addAdvertisement(a);
-                setState(() {
-                  _selectedType = null;
-                });
-              } else {
-                print('Lütfen bir ilan türü seçiniz.');
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text('Hata'),
-                      content: Text('Lütfen bir ilan türü seçiniz.'),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: Text('Tamam'),
-                        ),
-                      ],
-                    );
                   },
-                );
-              }
-              _clearTextFields();
-            },
-            style: ElevatedButton.styleFrom(
-              foregroundColor: textColor,
-              backgroundColor: backgroundColor,
-              textStyle: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-              padding: EdgeInsets.symmetric(horizontal: 118, vertical: 11),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
+                  items: ad_types.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value, style: TextStyle(color: textColor, fontSize: 21)),
+                    );
+                  }).toList(),
+                  dropdownColor: backgroundColor,
+                  style: TextStyle(color: textColor, fontSize: 21),
+                  icon: Icon(Icons.arrow_drop_down, color: textColor),
+                  iconSize: 24,
+                  elevation: 16,
+                  isExpanded: true,
+                ),
               ),
-              elevation: 10, // Shadow depth
             ),
-            child: Text('İlan Oluştur'),
-          ),
-
-        ],
+            SizedBox(height: 20),
+            //FOTO EKLEME YERİ
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.add_photo_alternate, size: 50),
+                  onPressed: () {
+                    // Functionality to be implemented later
+                    print('Icon to add image pressed');
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Add validation check here
+                if (_formKey.currentState!.validate()) {
+                  // Only save data if the form is valid
+                  String? userId = FirebaseAuth.instance.currentUser?.uid;
+                  if (userId == null) {
+                    print('No user logged in');
+                    return;
+                  }
+                  Advertisement a = Advertisement(
+                      advertisementName: _advertisementNameController.text,
+                      advertisementType: _selectedType,
+                      advertisementDetails: _advertisementDetailsController.text,
+                      userId: userId
+                  );
+                  AdvertisementService().addAdvertisement(a);
+                  _clearTextFields();
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                foregroundColor: textColor,
+                backgroundColor: backgroundColor,
+                textStyle: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                padding: EdgeInsets.symmetric(horizontal: 118, vertical: 11),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                elevation: 10,
+              ),
+              child: Text('İlan Oluştur'),
+            ),
+          ],
+        ),
       ),
     );
   }
+
 }
 
 
@@ -341,6 +317,9 @@ class _EtkinlikWidgetState extends State<EtkinlikWidget> {
   TextEditingController _organizerController = TextEditingController();
   TextEditingController _locationController = TextEditingController();
   TextEditingController _additionalInfoController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
 
   void _clearTextFields() {
     _eventNameController.clear();
@@ -575,10 +554,7 @@ class _EtkinlikWidgetState extends State<EtkinlikWidget> {
     );
   }
 
-
-
 }
-
 
 ///
 class DuyuruWidget extends StatefulWidget {
